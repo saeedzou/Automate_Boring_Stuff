5564e334baf3d9d3f466cf2d4a69e37ec2593fda
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2022-10-02 19:41:43 +0330

    Update login_automation_for_Sharif_university_loginpage.py

--- login_automation_for_Sharif_university_loginpage.py
+++ login_automation_for_Sharif_university_loginpage.py
@@ -1,20 +1,22 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-def login(username, password, login_url):
-    driver = webdriver.Edge()
+def login(username, password, login_url, driver):
     driver.minimize_window()
     try:
         driver.get(login_url)
-        driver.find_element_by_name("username").send_keys(username)
-        driver.find_element_by_name("password").send_keys(password)
-        driver.find_element_by_class_name("login100-form-btn").click()
+        driver.find_element(by=By.NAME, value='username').send_keys(username)
+        driver.find_element(by=By.NAME, value='password').send_keys(password)
+        driver.find_element(by=By.CLASS_NAME, value="login100-form-btn").click()
     finally:
         driver.quit()
+        quit()
 
     if driver is not None:
         driver.quit()
+        quit()
 
 
 if __name__ == '__main__':
@@ -22,5 +24,9 @@ if __name__ == '__main__':
     USERNAME = "user"
     PASSWORD = "pass"
     LOGIN_URL = "https://net2.sharif.edu/login"
-    login(USERNAME, PASSWORD, LOGIN_URL)
-
+    OPTIONS = webdriver.EdgeOptions()
+    #OPTIONS.add_argument('--disable-gpu')
+    OPTIONS.add_argument('--disable-dev-shm-usage')
+    OPTIONS.add_experimental_option('excludeSwitches', ['enable-logging'])
+    DRIVER = webdriver.Edge(options=OPTIONS)
+    login(USERNAME, PASSWORD, LOGIN_URL, DRIVER)
